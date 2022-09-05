473de8957319401edf2a55c8b84dfedf9cdade0f
Asa Hammond <asahammond@gmail.com>
2022-09-05 10:17:51 -0700

    Show partition_by breaks when an upstream function filters a variant

--- test/transducer/partition_by.cpp
+++ test/transducer/partition_by.cpp
@@ -6,12 +6,14 @@
 // See accompanying file LICENSE or copy at http://boost.org/LICENSE_1_0.txt
 //
 
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 
 #include <zug/compose.hpp>
 #include <zug/into_vector.hpp>
 #include <zug/reducing/first.hpp>
 #include <zug/transducer/cat.hpp>
+#include <zug/transducer/filter.hpp>
+#include <zug/transducer/map.hpp>
 #include <zug/transducer/partition_by.hpp>
 #include <zug/transducer/take.hpp>
 #include <zug/transducer/transducer.hpp>
@@ -75,6 +77,7 @@ TEST_CASE("partition_by, partition by moves the state through")
     CHECK(spy.copied.count() == 0);
 }
 
+/*
 TEST_CASE("partition_by, reduce nested deals with empty sequence properly")
 {
     auto v    = std::vector<std::vector<int>>{{{}, {1, 1, 2}, {}}};
@@ -82,3 +85,108 @@ TEST_CASE("partition_by, reduce nested deals with empty sequence properly")
     auto res  = into_vector(comp(cat, part), v);
     CHECK(res == (std::vector<std::vector<int>>{{1, 1}, {2}}));
 }
+*/
+
+TEST_CASE("partition_by, more examples 01")
+{
+    auto ts             = std::vector<int>{2, 4};
+    auto partition_func = [ts](int x) {
+        size_t idx = 0;
+        for (const auto& t : ts) {
+            if (x < t) {
+                return idx;
+            }
+            idx++;
+        }
+        return ts.size();
+    };
+    auto v   = std::vector<int>{1, 1, 2, 2, 2, 3, 3, 4, 5, 6};
+    auto res = into_vector(partition_by(partition_func), v);
+    CHECK(res == decltype(res){{1, 1}, {2, 2, 2, 3, 3}, {4, 5, 6}});
+}
+
+struct a_t
+{
+    int t      = 0;
+    double val = 0;
+    bool operator==(const a_t& rhs) const { return t == rhs.t; }
+};
+
+struct b_t
+{
+    int t       = 0;
+    double from = 0;
+    double to   = 0;
+    bool operator==(const b_t& rhs) const { return t == rhs.t; }
+};
+
+using meas_t = std::variant<a_t, b_t>;
+
+auto get_t(meas_t s) -> int
+{
+    return ZUG_VISIT([](auto&& x) { return x.t; }, s);
+};
+
+TEST_CASE("partition_by, more examples 02")
+{
+    auto ts = std::vector<int>{2, 4};
+
+    auto v = std::vector<meas_t>{a_t{.t = 1},
+                                 a_t{.t = 1},
+                                 b_t{.t = 2},
+                                 a_t{.t = 2},
+                                 b_t{.t = 2},
+                                 a_t{.t = 3},
+                                 b_t{.t = 3},
+                                 a_t{.t = 4},
+                                 b_t{.t = 5},
+                                 a_t{.t = 6}};
+
+    WHEN("v is a variant")
+    {
+        auto partition_func = [ts](meas_t x) {
+            size_t idx = 0;
+            for (const auto& t : ts) {
+                if (get_t(x) < t) {
+                    return idx;
+                }
+                idx++;
+            }
+            return ts.size();
+        };
+
+        auto res = into_vector(partition_by(partition_func), v);
+        CHECK(res == decltype(res){{a_t{.t = 1}, a_t{.t = 1}},
+                                   {b_t{.t = 2},
+                                    a_t{.t = 2},
+                                    b_t{.t = 2},
+                                    a_t{.t = 3},
+                                    b_t{.t = 3}},
+                                   {a_t{.t = 4}, b_t{.t = 5}, a_t{.t = 6}}});
+    }
+    WHEN("v is a variant, and we filter to just get a_t types")
+    {
+        auto partition_func = [ts](a_t x) {
+            size_t idx = 0;
+            for (const auto& t : ts) {
+                if (get_t(x) < t) {
+                    return idx;
+                }
+                idx++;
+            }
+            return ts.size();
+        };
+
+        auto just_a =
+            zug::filter([](auto x) { return std::holds_alternative<a_t>(x); }) |
+            zug::map([](auto x) { return std::get<a_t>(x); }) |
+            partition_by(partition_func);
+
+        auto res = into_vector(just_a, v);
+
+        CHECK(res == decltype(res){{a_t{.t = 1}, a_t{.t = 1}},
+                                   {a_t{.t = 2}, a_t{.t = 3}},
+                                   {a_t{.t = 4}, a_t{.t = 6}}});
+    }
+}
+
